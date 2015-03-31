5254553ec24afd25aa1af80b871d74e42a16dd6a
NIKHAT BHAYANI <NIKHATALY@GMAIL.COM>
2015-03-31 15:16:56 -0400

    adding exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,17 @@
+#Calculate a tip for a meal
+tip = 0.15*55
+puts tip
+
+#covert integer to string before combining two strings
+puts "x " + 55.to_s
+
+#string interpolation
+x = 45628 * 7839
+puts "the product of the two numbers is #{x}"
+
+#false||false||!(false)
+#[false||true]
+#true
+
+#The following statement should be true
+puts (true && false) || (false && true) || !(false && false)
